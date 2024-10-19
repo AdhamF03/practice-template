7fc9430fe76e7fd45730c3c51cfe8018c628f660
Adham <adham.fahmy21d@eslsca.edu.eg>
2024-10-19 11:42:45 +0300

    finished footer still on navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { ArtistryComponent } from './components/pages/artistry/artistry.component';
-import { TestimonialComponent } from './components/pages/testimonial/testimonial.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    NavbarComponent,
-    HomeComponent,
-    FooterComponent,
-    ArtistryComponent,
-    TestimonialComponent,
-  ],
+  imports: [RouterOutlet, NavbarComponent, HomeComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
