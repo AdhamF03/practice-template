3f635c22965b5749906fe3b4485e1df076e0ba4e
MKHX93 <mido.kh.06@gmail.com>
2024-10-19 11:18:50 +0300

    About page & shared component for scroll-to-top

--- src/app/components/pages/about/about.component.ts
+++ src/app/components/pages/about/about.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { ScrollToTopComponent } from "../../shared/scroll-to-top/scroll-to-top.component";
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [ScrollToTopComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
